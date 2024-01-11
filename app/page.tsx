86e48119853cc03877d679dc04d234ae8d1a0eae
Kay-El-41 <z.khantolino@gmail.com>
2024-01-11 17:00:49 +0630

    :card_file_box: Added a server action
    Added a server action when user submit the form, add a data to the database.

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,3 @@
 export default function Home() {
-  return (
-    <form>
-      <h3 className="font-bold m-3">Create a Snippet</h3>
-      <div className="flex flex-col gap-4">
-        <div className="flex flex-col">
-          <label htmlFor="title" className="w-12">
-            Title
-          </label>
-          <input
-            type="text"
-            name="title"
-            id="title"
-            className="border rounded p-2 w-full"
-          />
-        </div>
-        <div className="flex flex-col">
-          <label htmlFor="title" className="w-12">
-            Code
-          </label>
-          <textarea
-            name="title"
-            id="title"
-            className="border rounded p-2 w-full"
-          />
-        </div>
-      </div>
-    </form>
-  );
+  return <div>Home</div>;
 }
--- app/snippets/new/page.tsx
+++ app/snippets/new/page.tsx
@@ -1,18 +1,41 @@
 import React from "react";
+import { db } from "@/app/db";
+import { redirect } from "next/navigation";
 
 const SnippetCreatePage = () => {
+  async function createSnippet(formData: FormData) {
+    // Ths needs to be a server action!
+    "use server";
+    //? "use server" is a NEXT directive that this will use the server
+
+    // Check the user's input and make sure they are valid
+    const title = formData.get("title") as string;
+    const code = formData.get("code") as string;
+    // Create a new record in a database
+    const snippet = await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+    console.log(snippet);
+    // Redirect the user back to the root route
+    redirect("/");
+  }
+
   return (
-    <form>
+    <form action={createSnippet}>
       <h3 className="font-bold m-3">Create a Snippet</h3>
       <div className="flex flex-col gap-4">
         <div className="flex flex-col">
-          <label htmlFor="title" className="w-12">
+          <label htmlFor="code" className="w-12">
             Title
           </label>
           <input
+            required
+            id="title"
             type="text"
             name="title"
-            id="title"
             className="border rounded p-2 w-full"
           />
         </div>
@@ -21,8 +44,9 @@ const SnippetCreatePage = () => {
             Code
           </label>
           <textarea
-            name="code"
+            required
             id="code"
+            name="code"
             className="border rounded p-2 w-full"
           />
         </div>
