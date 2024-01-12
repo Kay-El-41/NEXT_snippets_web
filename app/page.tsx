a8fbdae92191098d8cc78ae87a04769ef9e89bbd
Kay-El-41 <z.khantolino@gmail.com>
2024-01-12 23:08:44 +0630

    :sparkles: Added Dynamic Route for Viewing Snippet

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
-export default function Home() {
-  return <div>Home</div>;
+import { db } from "@/app/db";
+
+export default async function Home() {
+  const snippets = await db.snippet.findMany();
+
+  const renderedSnippets = snippets.map((snippet) => {
+    return (
+      <div key={snippet.id}>
+        <h2>{snippet.title}</h2>
+      </div>
+    );
+  });
+
+  return <div>{renderedSnippets}</div>;
 }
